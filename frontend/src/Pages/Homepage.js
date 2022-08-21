b7ca5ccf06b423c71a285a4c4228fb6f06bf7c67
Xiaojun Lin <xiaojunlin@XiaojundeMacBook-Pro.local>
2022-08-21 12:48:26 -0700

    setup login and signup components UI with ChakraUI

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -1,8 +1,48 @@
 import React from 'react'
+import { Container, Box, Text } from '@chakra-ui/react'
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
+import Login from '../components/Authentication/Login'
+import Signup from '../components/Authentication/Signup'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <Container maxW="xl" centerContent>
+      <Box
+        d='flex'
+        justifyContent='center'
+        p={3}
+        bg='white'
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius='lg'
+        borderWidth='1px'
+      >
+        <Text fontSize='4xl' fontFamily='Work Sans' color='black'>TALKATIVE</Text>
+      </Box>
+      <Box
+        bg='white'
+        w='100%'
+        p={4}
+        borderRadius='lg'
+        borderWidth='1px'
+
+      >
+        <Tabs variant='soft-rounded' colorScheme='purple'>
+          <TabList mb='1em'>
+            <Tab width='50%'>Login</Tab>
+            <Tab width='50%'>Signup</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login /> 
+            </TabPanel>
+            <TabPanel>
+              <Signup />
+            </TabPanel>
+          </TabPanels>
+      </Tabs>
+      </Box>
+    </Container>
   )
 }
 
